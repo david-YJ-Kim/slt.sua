10d8d23bd00f88f8274025e063d12e0e2620869a
david-YJ-Kim <yj.david.kim@gmail.com>
2023-03-22 21:34:41 +0900

    query service
    query service

---
+++ src/main/java/com/plexter/sellter/agent/main/controller/QueryServiceController.java
@@ -0,0 +1,33 @@
+package com.plexter.sellter.agent.main.controller;
+
+import com.plexter.sellter.agent.main.mapper.QueryServiceMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class QueryServiceController {
+
+
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+
+    @PostMapping("/queryService")
+    public List<Map<String, Object>> executeQuery(@RequestBody String query){
+        try(SqlSession sqlSession = sqlSessionFactory.openSession()){
+            QueryServiceMapper mapper = sqlSession.getMapper(QueryServiceMapper.class);
+            System.out.println(query);
+
+            return mapper.execute(query);
+        }
+
+
+    }
+}
+
---
+++ src/main/java/com/plexter/sellter/agent/main/mapper/QueryServiceMapper.java
@@ -0,0 +1,14 @@
+package com.plexter.sellter.agent.main.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface QueryServiceMapper {
+
+    @Select("${query}")
+    List<Map<String, Object>> execute(String query);
+}
